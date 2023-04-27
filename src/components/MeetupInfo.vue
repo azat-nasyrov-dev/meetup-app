56df1b52105fd34b78ed9325ec7a157bf156328c
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-04-27 19:26:47 +0600

    added two utility functions, also added one component

---
+++ src/components/MeetupInfo.vue
@@ -0,0 +1,82 @@
+<template>
+  <ul class="meetup-info">
+    <li>
+      <UiIcon icon="user" class="meetup-info__icon" />
+      {{ organizer }}
+    </li>
+    <li>
+      <UiIcon icon="map" class="meetup-info__icon" />
+      {{ place }}
+    </li>
+    <li>
+      <UiIcon icon="cal-lg" class="meetup-info__icon" />
+      <time :datetime="isoDate">{{ localDate }}</time>
+    </li>
+  </ul>
+</template>
+
+<script>
+import UiIcon from './UiIcon.vue';
+import { formatAsIsoDate, formatAsLocalDate } from '../utils/dateUtils';
+
+export default {
+  name: 'MeetupInfo',
+
+  components: {
+    UiIcon,
+  },
+
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+
+    place: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  computed: {
+    isoDate() {
+      return formatAsIsoDate(this.date);
+    },
+
+    localDate() {
+      return formatAsLocalDate(this.date);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.meetup-info {
+  margin: 0;
+  padding: 0;
+}
+
+.meetup-info li {
+  list-style: none;
+  position: relative;
+  padding-left: 36px;
+  font-size: 18px;
+  line-height: 28px;
+  margin: 0 0 8px;
+}
+
+.meetup-info li:last-child {
+  margin: 0;
+}
+
+.meetup-info__icon {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+</style>
