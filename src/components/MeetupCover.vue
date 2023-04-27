7698155860c9dac4deaf20e957b9ad6028c0011b
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-04-28 03:25:51 +0600

    added two stateless components

---
+++ src/components/MeetupCover.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="meetup-cover">
+    <h1 class="meetup-cover__title">{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  name: 'MeetupCover',
+
+  props: {
+    title: { type: String },
+    image: { type: String },
+  },
+
+  setup(props) {
+    const backgroundImage = computed(() => (props.image ? `url('${props.image}')` : 'var(--default-cover)'));
+
+    return {
+      backgroundImage,
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* _meetup-cover.css */
+
+.meetup-cover {
+  background-size: cover;
+  background-position: center;
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind(backgroundImage);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 410px;
+  max-width: 1216px;
+  margin: 0 auto;
+}
+
+.meetup-cover__title {
+  color: var(--white);
+  font-family: Roboto, sans-serif;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 48px;
+  padding: 0 16px;
+  text-align: center;
+}
+
+@media all and (min-width: 992px) {
+  .meetup-cover__title {
+    font-size: 72px;
+    line-height: 84px;
+  }
+}
+</style>
---
+++ src/components/MeetupDescription.vue
@@ -0,0 +1,26 @@
+<template>
+  <p class="meetup-description">{{ description }}</p>
+</template>
+
+<script>
+export default {
+  name: 'MeetupDescription',
+
+  props: {
+    description: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* _meetup-description.css */
+.meetup-description {
+  padding-top: 33px;
+  margin-bottom: 24px;
+  font-size: 18px;
+  line-height: 28px;
+  white-space: pre-wrap;
+}
+</style>
