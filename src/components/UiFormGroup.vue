a9dd03340c45c6ed40394ce494f101caefcc86ff
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-04-29 02:03:13 +0600

    added components UiForm, UiFormGroup

---
+++ src/components/UiFormGroup.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form-group" :class="{ 'form-group_inline': inline }">
+    <label>
+      <span v-if="label" class="form-group__label">{{ label }}</span>
+      <slot />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiFormGroup',
+
+  props: {
+    inline: {
+      type: Boolean,
+      default: false,
+    },
+
+    label: String,
+  },
+};
+</script>
+
+<style scoped>
+/* _form-group.css */
+.form-group {
+  position: relative;
+  margin-bottom: 24px;
+}
+
+.form-group.form-group_inline {
+  display: inline-block;
+  margin-bottom: 0;
+}
+
+.form-group.form-group_inline + .form-group.form-group_inline {
+  margin-left: 16px;
+}
+
+.form-group__label {
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
+  color: var(--body-color);
+  margin-bottom: 10px;
+  display: block;
+}
+</style>
