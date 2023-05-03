8c0d72e6c21fb351456b4d33135882eebb1865e3
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-05-03 10:27:40 +0600

    added two components, MeetupsList, MeetupsLogo

---
+++ src/components/MeetupsList.vue
@@ -0,0 +1,53 @@
+<template>
+  <UiTransitionGroupFade tag="ul" class="meetups-list">
+    <li v-for="meetup in meetups" :key="meetup.id" class="meetups-list__item">
+      <RouterLink
+        :to="{ name: 'meetup', params: { meetupId: meetup.id } }"
+        class="meetups-list__item-link"
+        tabindex="0"
+      >
+        <MeetupCard :meetup="meetup" />
+      </RouterLink>
+    </li>
+  </UiTransitionGroupFade>
+</template>
+
+<script>
+import UiTransitionGroupFade from './UiTransitionGroupFade.vue';
+import MeetupCard from './MeetupCard.vue';
+
+export default {
+  name: 'MeetupsList',
+
+  components: {
+    UiTransitionGroupFade,
+    MeetupCard,
+  },
+
+  props: {
+    meetups: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* _meetups-list.css */
+.meetups-list {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.meetups-list__item {
+  margin: 0 0 32px 0;
+  text-decoration: none;
+}
+
+.meetups-list__item-link {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
