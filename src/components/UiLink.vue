b417118f96c964828a75a80aa49c9a878a4b7d83
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-05-10 18:15:41 +0600

    added a couple of reusable components

---
+++ src/components/UiLink.vue
@@ -0,0 +1,28 @@
+<template>
+  <component :is="tag" class="link"><slot /></component>
+</template>
+
+<script>
+export default {
+  name: 'UiLink',
+
+  props: {
+    tag: {
+      type: [String, Object, Function],
+      default: 'router-link',
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* _link.css */
+.link {
+  color: var(--blue);
+  text-decoration: none;
+}
+
+.link:hover {
+  text-decoration: underline;
+}
+</style>
