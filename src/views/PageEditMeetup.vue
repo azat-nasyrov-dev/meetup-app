9fc5f8f06e4278b1fe414df6b368e68fa682fea8
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-05-25 22:50:14 +0600

    added logic to PageEditMeetup page

--- src/views/PageEditMeetup.vue
+++ src/views/PageEditMeetup.vue
@@ -1,17 +1,56 @@
 <template>
-  <div class="page-edit">
-    Page edit meetup will be here!
-  </div>
+  <MeetupForm
+    v-if="meetup"
+    :meetup="meetup"
+    submit-text="Сохранить"
+    @submit="handleSubmit"
+    @cancel="$router.push({ name: 'meetup', params: { meetupId: meetupId } })"
+  />
+  <UiContainer v-else>
+    <UiAlert>Загрузка...</UiAlert>
+  </UiContainer>
 </template>
 
 <script>
+import { useHead } from '@vueuse/head';
+import LayoutMeetupForm from '../components/LayoutMeetupForm.vue';
+import MeetupForm from '../components/MeetupForm.vue';
+import UiAlert from '../components/UiAlert.vue';
+import UiContainer from '../components/UiContainer.vue';
+import { useLayout } from '../composables/useLayout.js';
+import { useMeetupFormSubmit } from '../composables/useMeetupFormSubmit.js';
+import { useMeetupFetch } from '../composables/useMeetupFetch.js';
+
 export default {
   name: 'PageEditMeetup',
+
+  components: {
+    UiAlert,
+    UiContainer,
+    MeetupForm,
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    useLayout(LayoutMeetupForm, { title: 'Редактирование митапа' });
+    useHead({ title: 'Редактирование митапа' });
+
+    const { meetup } = useMeetupFetch(props.meetupId);
+
+    const handleSubmit = useMeetupFormSubmit('edit');
+
+    return {
+      meetup,
+      handleSubmit,
+    };
+  },
 };
 </script>
 
-<style scoped>
-.page-edit {
-  text-align: center;
-}
-</style>
+<style scoped></style>
