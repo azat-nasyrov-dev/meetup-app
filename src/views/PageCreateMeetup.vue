0312960b7d563231d4c2f651f1e3495592869a9a
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-05-25 22:29:12 +0600

    added logic to PageCreateMeetup page

--- src/views/PageCreateMeetup.vue
+++ src/views/PageCreateMeetup.vue
@@ -1,17 +1,37 @@
 <template>
-  <div class="page-create">
-    Page create meetup will be here!
-  </div>
+  <MeetupForm :meetup="meetup" submit-text="Создать" @submit="handleSubmit" @cancel="$router.push({ name: 'index' })" />
 </template>
 
 <script>
+import { ref } from 'vue';
+import { useHead } from '@vueuse/head';
+import LayoutMeetupForm from '../components/LayoutMeetupForm.vue';
+import MeetupForm from '../components/MeetupForm.vue';
+import { createMeetup } from '../services/meetupService.js';
+import { useLayout } from '../composables/useLayout.js';
+import { useMeetupFormSubmit } from '../composables/useMeetupFormSubmit.js';
+
 export default {
   name: 'PageCreateMeetup',
+
+  components: {
+    MeetupForm,
+  },
+
+  setup() {
+    useLayout(LayoutMeetupForm, { title: 'Создание митапа' });
+    useHead({ title: 'Создание митапа' });
+
+    const meetup = ref(createMeetup());
+
+    const handleSubmit = useMeetupFormSubmit('create');
+
+    return {
+      meetup,
+      handleSubmit,
+    };
+  },
 };
 </script>
 
-<style scoped>
-.page-create {
-  text-align: center;
-}
-</style>
+<style scoped></style>
